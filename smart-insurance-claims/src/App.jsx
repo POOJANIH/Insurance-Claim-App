54437044243d8243f1b0ffb6e0546336d3607fca
Minoli <POOJANIH@our.ecu.edu.au.com>
2024-12-29 00:14:27 +0530

    Initial commit - Setup React app with Vite

---
+++ smart-insurance-claims/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './components/header/Header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+    <Header/>
+    <Home />
+    <About/>
+      
+
+    </div>
+  )
+}
+
+export default App
